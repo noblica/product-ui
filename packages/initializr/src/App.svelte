22b7485e33059937aa9a87b1f13c5abfa89df97f
Dusan Perkovic <dusan.perkovic@axoniq.io>
2021-07-07 13:33:11 +0200

    Remove console.log from intializr.

--- packages/initializr/src/App.svelte
+++ packages/initializr/src/App.svelte
@@ -13,8 +13,6 @@ let dataLoaded = false;
 let initialData: InitializerData;
 let addedDependencies: DependenciesValue[] = [];
 
-console.log('test app');
-
 onMount(async() => {
 	initialData = await getInitialData();
 
